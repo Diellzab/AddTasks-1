97b1a9b0ee628a7e648bd12b92259546ffd5adc5
dardan bakiu <dardanbakiu@dardans-iMac.local>
2020-07-03 01:16:18 -0700

    Initial Commit

---
+++ addtasks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  addtasks
+//
+//  Created by dardan bakiu on 7/3/20.
+//  Copyright © 2020 dardan bakiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
